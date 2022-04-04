9d7fed0426bea9845ded57c73a048c02f99b52d2
NatalieClark2021 <NatalieClark2021@gmail.com>
2022-04-04 17:35:59 -0500

    week13 excercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dynamic-button-event-binding';
+  isDisabled: boolean  = true;
 }
